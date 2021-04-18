334247a434d161fae29e9a5682a889c132ac1dee
BettyHoPro <bettyvisualtech@gmail.com>
2021-04-17 21:02:28 -0700

    feat: get and stop audio from laptop

--- src/components/Visualization.js
+++ src/components/Visualization.js
@@ -8,7 +8,20 @@ export default class Visualization extends Component {
   };
  }
 
+ // get laptop's speaker voice
+ async getAudioFromLaptop() {
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    });
+    this.setState({ audio });
+ }
   
+ stopAudioFromLaptop() {
+  // this.state.audio.getTracks().forEach(track => track.stop());
+  this.setState({ audio: null });
+ }
+
   render() {
     return (
       <div className="Visualization">
