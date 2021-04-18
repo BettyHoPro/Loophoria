d170ab884c19dce124e4f9e85ab71ce5c1ba80a4
BettyHoPro <bettyvisualtech@gmail.com>
2021-04-17 21:24:18 -0700

    feat: basic visual compoent app

---
+++ src/components/AnylsAudio.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class AnylsAudio extends Component{
+
+   constructor(props) {
+     super(props);
+     this.state
+   }
+}
--- src/components/Visualization.js
+++ src/components/Visualization.js
@@ -1,33 +1,41 @@
-import React, { Component }from "react";
+import React, { Component } from "react";
 import "./Visualization.css";
+import "./Button.css";
 export default class Visualization extends Component {
- constructor(props){
-  super(props);
-  this.state = {
-    audio: null
-  };
- }
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: null,
+    };
+     //bind context to component
+     this.switchMode = this.switchMode.bind(this);
+  }
 
- // get laptop's speaker voice
- async getAudioFromLaptop() {
+  // get laptop's speaker voice
+  async getAudioFromLaptop() {
     const audio = await navigator.mediaDevices.getUserMedia({
       audio: true,
-      video: false
+      video: false,
     });
     this.setState({ audio });
- }
-  
- stopAudioFromLaptop() {
-  // this.state.audio.getTracks().forEach(track => track.stop());
-  this.setState({ audio: null });
- }
+  }
+
+  stopAudioFromLaptop() {
+    //console.log(this.state);
+    this.state.audio.getTracks().forEach(track => track.stop());
+    this.setState({ audio: null });
+  }
+
+  switchMode() {
+    this.state.audio ? this.stopAudioFromLaptop() : this.getAudioFromLaptop();
+  }
 
   render() {
     return (
-      <div className="Visualization">
-      
-       
-      </div>
-    );
-  }
+    <div className="Visualization">
+      <button class={'btn-loop'} onClick={this.switchMode}>
+        {this.state.audio ? "Stop" : "Visual"}
+      </button>
+    </div>
+    )}
 }
